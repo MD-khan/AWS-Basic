afd3e6b11c97687a6d247c573b51eb13b6425270
MD-khan <md.monir.khan707@gmail.com>
2021-12-31 20:56:44 -0500

    Updated the script
    The script will:
    
    Install an Apache web server (httpd)
    Configure the web server to automatically start on boot
    Activate the Web server
    Create a simple web page

--- EC2-user-data.sh
+++ EC2-user-data.sh
@@ -2,4 +2,4 @@
 yum -y install httpd
 systemctl enable httpd
 systemctl start httpd
-echo '<html><h1>Hello Welcome To EC2 Web Server!</h1></html>' > /var/www/html/index.html
+echo '<html><h1>Hello, Welcome To EC2 Web Server!</h1></html>' > /var/www/html/index.html
